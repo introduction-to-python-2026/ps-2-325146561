3537cc1f23d485d5834217ab1d0cd7412cfe9c66
325146561 <noasela2002@gmail.com>
2025-10-30 13:29:43 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,5 +1,15 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
+    def find_max_number(n1, n2, n3):
+    if n1 > n2 :
+      if n1 > n3 :
+        return n1 
+      else :
+        return n3
+    else :
+      if n2 > n3 :
+        return n2
+      else :
+        return n3
 
 def find_mean(num1, num2, num3):
     pass  # Replace 'pass' with code
